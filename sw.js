59d0be46e0d99e9d4181cbf40c155f03afa8624f
Prathvii <truesyanic@proton.me>
2025-11-22 00:07:31 +0530

    Initial Game Upload :)

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'flying-modi-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './bg.webp',
+  './bird.png',
+  './pipe.webp',
+  './music.mp3',
+  './gameover.mp3',
+  './fly.mp3',
+  './manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
